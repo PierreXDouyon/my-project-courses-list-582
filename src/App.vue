57d34cdb4a980fe1c397ebd005fa634b61150010
Pierre Douyon <pierredouyon514@gmail.com>
2023-08-02 22:11:17 -0400

    Installed vue.js

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Pierre Vue.js App" />
+  <HelloWorld msg="Welcome to Pierre Douyon Vue.js App" />
 </template>
 
 <script>
