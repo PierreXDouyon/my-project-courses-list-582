0e14908ee45169ea59f5bc3dc5e84ae36aed9491
Pierre Douyon <pierredouyon514@gmail.com>
2023-08-02 22:17:19 -0400

    +CourseItem.vue & App.vue, student array displays

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,36 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Pierre Douyon Vue.js App" />
+  <div>
+    <h1>Course List App</h1>
+    <p>{{ count }}</p>
+    <CourseItem1
+      v-for="student in students"
+      :key="student.id"
+      :given="student"
+    />
+  </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import CourseItem1 from "./components/CourseItem1.vue";
 
 export default {
   name: "App",
+  data() {
+    return {
+      students: [
+        { id: 1, name: "patrick", credits: 22 },
+        { id: 2, name: "john", age: 45 },
+        { id: 3, name: "mary", age: 77 },
+      ],
+      count: 0,
+    };
+  },
   components: {
-    HelloWorld,
+    CourseItem1,
   },
 };
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+/* Styles if any */
 </style>
---
+++ src/components/CourseItem1.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div>Course Name: {{ given.name }}</div>
+    <p>Course ID: {{ given.id }}</p>
+    <p>Credits: {{ given.credits }}</p>
+    <button @click="innerRemove">remove</button>
+    <button @click="outerAdd">Add to Parent</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CourseItem1",
+  props: {
+    given: {
+      type: Object,
+      required: true,
+    },
+  },
+  methods: {
+    innerRemove() {
+      // Logic to handle removing a course, if needed.
+      // For now, this method is commented out to avoid confusion with the original code.
+      // this.$emit("remove-course");
+    },
+    outerAdd() {
+      this.$emit("count-to-parent");
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+/* Styles if any */
+</style>
